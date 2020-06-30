511a199d4a56711773931af4a618f5cddf23a21e
dimasm93 <software.dimas_m@icloud.com>
2020-06-30 23:19:37 +0700

    membuat insert, update, delete menggunakan prepared statement

--- src/main/java/com/maryanto/dimas/example/dao/ExampleTableDao.java
+++ src/main/java/com/maryanto/dimas/example/dao/ExampleTableDao.java
@@ -17,17 +17,60 @@ public class ExampleTableDao implements CrudRepository<ExampleTable, String> {
 
     @Override
     public ExampleTable save(ExampleTable value) throws SQLException {
-        return null;
+        String query = "insert into example_table (id, name, created_date, created_time, is_active, counter, currency, description, floating)\n" +
+                "values (uuid_generate_v4(), ?, ?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        statement.setString(1, value.getName());
+        statement.setDate(2, value.getCreatedDate());
+        statement.setTimestamp(3, value.getCreatedTime());
+        statement.setBoolean(4, value.getActive());
+        statement.setInt(5, value.getCounter());
+        statement.setBigDecimal(6, value.getCurrency());
+        statement.setString(7, value.getDescription());
+        statement.setDouble(8, value.getFloating());
+        int row = statement.executeUpdate();
+        ResultSet generatedKeys = statement.getGeneratedKeys();
+        if (generatedKeys.next())
+            value.setId(generatedKeys.getString("id"));
+        statement.close();
+        return value;
     }
 
     @Override
     public ExampleTable update(ExampleTable value) throws SQLException {
-        return null;
+        //language=PostgreSQL
+        String query = "update example_table\n" +
+                "set name        = ?,\n" +
+                "    is_active   = ?,\n" +
+                "    counter     = ?,\n" +
+                "    currency    = ?,\n" +
+                "    description = ?,\n" +
+                "    floating    = ?\n" +
+                "where id = ?";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setString(1, value.getName());
+        statement.setBoolean(2, value.getActive());
+        statement.setInt(3, value.getCounter());
+        statement.setBigDecimal(4, value.getCurrency());
+        statement.setString(5, value.getDescription());
+        statement.setDouble(6, value.getFloating());
+        statement.setString(7, value.getId());
+        int row = statement.executeUpdate();
+        statement.close();
+        return value;
     }
 
     @Override
     public Boolean removeById(String value) throws SQLException {
-        return null;
+        //language=PostgreSQL
+        String query = "delete\n" +
+                "from example_table\n" +
+                "where id = ?";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setString(1, value);
+        int row = statement.executeUpdate();
+        statement.close();
+        return row >= 1;
     }
 
     @Override
--- src/test/java/com/maryanto/dimas/example/TestExampleTableDao.java
+++ src/test/java/com/maryanto/dimas/example/TestExampleTableDao.java
@@ -9,8 +9,13 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import javax.sql.DataSource;
+import java.math.BigDecimal;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -76,6 +81,56 @@ public class TestExampleTableDao extends TestCase {
         list = dao.findAll(2L, 4L, 0L, "desc", params);
         Assert.assertEquals("Jumlah data list pagination ke 2", 4, list.size());
 
+        connection.close();
+    }
+
+    @Test
+    public void testUpdateData() throws SQLException {
+        DataSource dataSource = this.config.getDataSource();
+        Connection connection = dataSource.getConnection();
+        log.info("status connected");
+        ExampleTableDao dao = new ExampleTableDao(connection);
+
+        ExampleTable example = new ExampleTable(
+                null,
+                "Gilang",
+                Date.valueOf(LocalDate.now()),
+                Timestamp.valueOf(LocalDateTime.now()),
+                true,
+                0,
+                BigDecimal.ONE,
+                "",
+                0d
+        );
+
+        example = dao.save(example);
+
+        log.info("data return after save: {}", example);
+        Assert.assertNotNull("Example id save", example.getId());
+        Optional<ExampleTable> optional = dao.findById(example.getId());
+        Assert.assertTrue("Data telah di simpan", optional.isPresent());
+
+        dao.removeById(example.getId());
+        optional = dao.findById(example.getId());
+        Assert.assertFalse("Data telah di hapus", optional.isPresent());
+
+        optional = dao.findById("002");
+        Assert.assertTrue("find by id 002 is present", optional.isPresent());
+
+        example = optional.orElse(new ExampleTable());
+        String oldName = "Muhamad yusuf";
+        Assert.assertEquals("Nama dari id 002 adalah ", oldName, example.getName());
+
+        example.setName("Muhamad Purwadi");
+        dao.update(example);
+
+        Optional<ExampleTable> optionalUpdated = dao.findById(example.getId());
+        ExampleTable newData = optionalUpdated.orElse(new ExampleTable());
+
+        Assert.assertEquals("Nama dari id 002 adalah ", example.getName(), newData.getName());
+
+        example.setName(oldName);
+        dao.update(example);
         connection.close();
     }
 }
