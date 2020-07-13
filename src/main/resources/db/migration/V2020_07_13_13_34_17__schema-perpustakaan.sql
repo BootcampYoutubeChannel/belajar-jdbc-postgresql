22f0fc69949d720900fd062556b81db945668bba
dimasm93 <software.dimas_m@icloud.com>
2020-07-13 13:53:52 +0700

    membuat project perpustakaan

---
+++ src/main/resources/db/migration/V2020_07_13_13_34_17__schema-perpustakaan.sql
@@ -0,0 +1,55 @@
+create schema perpustakaan;
+
+create table perpustakaan.buku
+(
+    id          character varying(64) not null primary key default uuid_generate_v4(),
+    nama        character varying(50) not null,
+    isbn        character varying(64),
+    penerbit_id character varying(64) not null
+);
+
+create table perpustakaan.penerbit
+(
+    id     character varying(64) not null primary key default uuid_generate_v4(),
+    nama   character varying(64) not null,
+    alamat text
+);
+
+alter table perpustakaan.buku
+    add constraint fk_penerbit_id foreign key (penerbit_id)
+        references perpustakaan.penerbit (id) on update cascade on delete cascade;
+
+create table perpustakaan.anggota
+(
+    id        character varying(64) not null primary key default uuid_generate_v4(),
+    nomor_ktp character varying(64) not null unique,
+    nama      character varying(50) not null,
+    alamat    text
+);
+
+create table perpustakaan.transaksi
+(
+    id                character varying(64) not null primary key default uuid_generate_v4(),
+    tanggal_transaksi date                  not null             default now(),
+    anggota_id        character varying(64) not null
+);
+
+alter table perpustakaan.transaksi
+    add constraint fk_anggota_id foreign key (anggota_id)
+        references perpustakaan.anggota (id) on update cascade on delete cascade;
+
+create table perpustakaan.transaksi_detail
+(
+    id              character varying(64) not null default uuid_generate_v4(),
+    transaksi_id    character varying(64) not null,
+    buku_id         character varying(64) not null,
+    tanggal_kembali date                  not null
+);
+
+alter table perpustakaan.transaksi_detail
+    add constraint fk_buku_id foreign key (buku_id)
+        references perpustakaan.buku (id) on update cascade on delete cascade;
+
+alter table perpustakaan.transaksi_detail
+    add constraint fk_transaksi_id foreign key (transaksi_id)
+        references perpustakaan.transaksi (id) on update cascade on delete cascade;
---
+++ src/main/resources/db/migration/V2020_07_13_13_47_05__data-dumy-perpustakaan.sql
@@ -0,0 +1,12 @@
+insert into perpustakaan.penerbit(id, nama, alamat)
+values ('001', 'Informatika', 'Jakarta'),
+       ('002', 'Erlangga', 'Bandung');
+
+insert into perpustakaan.buku(id, nama, isbn, penerbit_id)
+VALUES ('001', 'Pemograman Java', '0000-000-0000', '001'),
+       ('002', 'ELKS Pelajaran Bahasa Indonesia', '0001-000-0001', '002');
+
+insert into perpustakaan.anggota(id, nomor_ktp, nama, alamat)
+values ('001', '6204123434', 'Dimas Maryanto', 'Bandung'),
+       ('002', '6204123435', 'Muhamad Yusuf', 'Bandung'),
+       ('003', '6204123437', 'Muhamad Purwadi', 'Jakarta');
